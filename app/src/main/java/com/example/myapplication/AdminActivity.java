b88f9fbae1248e45fed6f8a2c34f4805ecaccbf1
duynguyen14 <duy68327@gmail.com>
2025-08-24 09:05:15 +0700

    demo

--- app/src/main/java/com/example/myapplication/AdminActivity.java
+++ app/src/main/java/com/example/myapplication/AdminActivity.java
@@ -39,8 +39,6 @@ public class AdminActivity extends AppCompatActivity {
         });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
